657e73ac1246edc2d7b8a4cf586500d60bc1ad4a
keinermendoza <keinermendoza@gmail.com>
2023-05-21 16:47:02 -0400

    create two really different logic-style for service cards, one for mobile other for desktop

--- assets/actions.js
+++ assets/actions.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded",() => {
+    document.querySelectorAll(".card-header").forEach((cardHeader) => {
+        cardHeader.addEventListener("click", () => {
+            // cardHeader.parentNode.classList.toggle("active");
+            var cardContent = cardHeader.nextElementSibling.classList.toggle("active")
+        });
+    });
+    
 
 });
