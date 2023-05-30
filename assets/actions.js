57916dab8981ebfb0ca28a639a53676e5c030a6d
keinermendoza <keinermendoza@gmail.com>
2023-05-30 12:03:04 -0400

    debug phone navbar, and first test google analitycs

--- assets/actions.js
+++ assets/actions.js
@@ -8,11 +8,45 @@ document.addEventListener("DOMContentLoaded",() => {
     
     // navbar
     // https://www.youtube.com/watch?v=dIFNHeHTja0&list=PLzBaHDKdyqYHOYsN9lMjfQgjRbnziuN7k&index=9
-    const hamburguer = document.querySelector(".hamburguer");
-    const navbar = document.querySelector(".nav");
-    hamburguer.onclick = () => {
-        navbar.classList.toggle("active");
-    }
+    // const hamburguer = document.querySelector(".hamburguer");
+    // const navbar = document.querySelector(".nav");
+    // hamburguer.onclick = () => {
+    //     navbar.classList.toggle("active");
+    // }
+
+    
+        const menuBtn = document.querySelector(".hamburguer");
+        const menuBtnIcon = document.querySelector(".hamburguer i");
+        const scrollingNavbar = document.querySelector(".nav");
+    
+    
+        menuBtn.onclick = function() {
+            if (scrollingNavbar.classList.contains("active")) {
+                CloseScrollingNavbar();
+            } else {
+                OpenScrollingNavbar();
+            }
+        }
+    
+        // cerrar navbar al cambiar window size
+        window.addEventListener("resize", CloseScrollingNavbar);
+    
+        // cerrar navbar al hacer click en cualquier link
+        document.querySelectorAll(".nav-link").forEach(function(link) {
+            link.onclick = CloseScrollingNavbar;
+        });
+    
+        // cerrar navbar
+        function CloseScrollingNavbar(){
+            scrollingNavbar.classList.remove("active");
+            menuBtnIcon.classList = "fa-solid fa-bars"
+        };
+    
+        // abrir navbar
+        function OpenScrollingNavbar(){
+            scrollingNavbar.classList.add("active");
+            menuBtnIcon.classList = "fa-solid fa-xmark"
+        };
 
     // formulario de contacto
     const formContacto = document.getElementById("form-contacto")
